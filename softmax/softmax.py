cfb98fe14b09b8c2ac812fd201f965cf9f94c96f
Phi-C <chenxjhit@163.com>
2025-02-20 23:43:57 +0800

    feat: add softmax impl

---
+++ softmax/softmax.py
@@ -0,0 +1,78 @@
+from pathlib import Path
+import torch
+import time
+from torch.utils.cpp_extension import load_inline
+
+
+def compile_extension():
+    cuda_source = Path(
+        "/workspace/Custom-Kernel-Impl/softmax/softmax_kernel.cu"
+    ).read_text()
+    cpp_source = "void softmax(torch::Tensor& input, torch::Tensor& output, const int m, const int n, const float eps);"
+
+    # Load the CUDA kernel as a PyTorch extension
+    softmax_extension = load_inline(
+        name="softmax_extension",
+        cpp_sources=cpp_source,
+        cuda_sources=cuda_source,
+        functions=["softmax"],
+        with_cuda=True,
+        extra_cuda_cflags=["-O2"],
+        build_directory='./cuda_build',
+    )
+    return softmax_extension
+
+  
+    
+def softmax_base_impl(input_tensor, m, n, eps=1e-9):
+    """Rotay position embedding baseline implmentation. Process One token and one head only.
+    
+    Note:
+        In https://github.com/huggingface/transformers/blob/main/src/transformers/models/llama/modeling_llama.py,
+        apply_rotary_pos_emb returns (data * cos) + (rotate_half(data) * sin), here sin and cos are repeated to
+        [num_tokens, head_dim]
+
+    Args:
+        input_tensor (torch.Tensor): input tensor, shape: (num_tokens, head_dim * head_num)
+        sin (torch.Tensor): sin param for input_tensor, shape: (num_tokens, head_dim // 2)
+        cos (torch.Tensor): cos param for input_tensor, shape: (num_tokens, head_dim // 2)
+        head_dim (int): dimension of each head, default: 128
+        head_num (int): number of heads, default: 32
+    Returns:
+        input_tensor (torch.Tensor): Tensor after rotary position embedding, shape: (num_tokens, head_dim * head_num)
+    """
+    return torch.nn.functional.softmax(input_tensor, dim=-1)
+
+
+def main():
+    """
+    Use torch cpp inline extension function to compile the kernel in grayscale_kernel.cu.
+    Read input image, convert it to grayscale via custom cuda kernel and write it out as png.
+    """
+    ext = compile_extension()
+    torch.manual_seed(42)
+    
+    m = 2
+    # FIX: n = 130, last two elements are inccorect
+    # n = 130
+    n = 128
+    eps = 1e-6
+    input = torch.randn(m, n, dtype=torch.float).cuda()
+    output = torch.zeros_like(input)
+    
+    t1 = time.time()
+    base_result = softmax_base_impl(input, m, n, eps)
+    print(f"Base Impl Time Cost: {time.time()-t1}")
+    t2 = time.time()
+    ext.softmax(input, output, m, n, eps)
+    print(f"Extension Time Cost: {time.time()-t2}")
+    print(f"input: {input}")
+
+    print(f"base result: {base_result}")
+    print(f"output: {output}")
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ softmax/softmax_kernel.cu
@@ -0,0 +1,101 @@
+#include <ATen/Dispatch.h>
+#include <c10/cuda/CUDAException.h>
+#include <c10/cuda/CUDAStream.h>
+
+__device__ float warpReduceMax(float val) {
+    for (int32_t offset = 32 / 2; offset > 0; offset >>= 1) {
+        val = max(val, __shfl_xor_sync(0xFFFFFFFF, val, offset));
+    }
+    return val;
+}
+
+__device__ float warpReduceSum(float val) {
+    for (int32_t offset = 32 / 2; offset > 0; offset >>= 1) {
+        val += __shfl_xor_sync(0xFFFFFFFF, val, offset);
+    }
+    return val;
+}
+
+__device__ void blockcompute(float* output, float& max_val, float& dom_val) {
+    __shared__ __align__(sizeof(float)) float s_max_data[32];
+    __shared__ __align__(sizeof(float)) float s_dom_data[32];
+
+    int32_t tid = threadIdx.x;
+    int32_t lane = threadIdx.x % 32; // 32 is warp size
+    int32_t wid = threadIdx.x / 32;
+    int32_t thres_tid = blockDim.x / 32 * 32;
+
+    float warp_max = warpReduceMax(max_val);
+    float warp_dom = warpReduceSum(dom_val * exp(max_val - warp_max));
+
+    if (lane == 0) {
+        s_max_data[wid] = warp_max;
+        s_dom_data[wid] = warp_dom;
+    }
+    __syncthreads();
+
+    // Here, we assume n <= 32 x 32, if n > 1024, block reduction is more
+    // complicated
+    warp_max = (lane < (blockDim.x + 31) / 32) ? s_max_data[lane] : -1000000.0;
+    warp_dom = (lane < (blockDim.x + 31) / 32) ? s_dom_data[lane] : 0.0;
+    max_val = warpReduceMax(warp_max);
+    dom_val = warpReduceSum(warp_dom * exp(warp_max - max_val));
+}
+
+template <typename dtype>
+__global__ void online_softmax(const dtype* input, dtype* output,
+                               const int32_t m, const int32_t n, float eps) {
+    int32_t tid = threadIdx.x;
+    int32_t bid = blockIdx.x;
+
+    for (int32_t i = bid; i < m; i += gridDim.x) {
+
+        // Pass 1: Find the maximum value and the dominator value
+        float max_val = input[i * n + tid];
+        float prev_max_val = max_val;
+        float dom_val = 0.0;
+        for (int32_t idx = tid; idx < n; idx += blockDim.x) {
+            prev_max_val = max_val;
+            max_val = max(prev_max_val, input[i * n + idx]);
+            dom_val = dom_val * exp(prev_max_val - max_val) +
+                      exp(input[i * n + idx] - max_val);
+        }
+        __syncthreads();
+
+        // Pass 1: Find the maximum value and the dominator value
+        // float max_val = input[i * n + tid];
+        // float dom_val = 0.0;
+        // for (int32_t idx = tid; idx < n; idx += blockDim.x) {
+        //     max_val = max(max_val, input[i * n + idx]);
+        // }
+        // __syncthreads();
+
+        // for (int32_t idx = tid; idx < n; idx += blockDim.x) {
+        //     dom_val += exp(input[i * n + idx] - max_val);
+        // }
+        // __syncthreads();
+
+        blockcompute(output, max_val, dom_val);
+
+        // Pass 2: Compute the softmax values
+        for (int32_t idx = tid; idx < n; idx += blockDim.x) {
+            output[i * n + idx] =
+                exp(input[i * n + idx] - max_val) / (dom_val + eps);
+        }
+    }
+}
+
+void softmax(torch::Tensor& input, torch::Tensor& output, const int m,
+             const int n, const float eps) {
+    assert(input.device().type() == torch::kCUDA);
+    assert(input.dim() == 2);
+
+    dim3 grid(m);
+    dim3 block(std::min(n, 1024));
+
+    const at::cuda::CUDAStream stream = at::cuda::getCurrentCUDAStream();
+    online_softmax<float><<<grid, block, 0, stream>>>(
+        input.data_ptr<float>(), output.data_ptr<float>(), m, n, eps);
+
+    C10_CUDA_KERNEL_LAUNCH_CHECK();
+}
